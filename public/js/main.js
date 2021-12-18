43e2f8e59d74545831cfb9352e9b9b878d6badfb
EneasLari <eneaslari@gmail.com>
2021-12-18 03:30:57 +0200

    chatting completed

--- public/js/main.js
+++ public/js/main.js
@@ -5,285 +5,285 @@ var isStarted = false;
 var pc;
 var turnReady;
 var datachannel;
+var clientName = "user" + Math.floor(Math.random() * 1000 + 1);
+var remoteclient;
+
+document.getElementById("yourname").innerHTML="You: "+clientName
 
 //Initialize turn/stun server here
 //turnconfig will be defined in public/js/config.js
 var pcConfig = turnConfig;
 
-
 // Prompting for room name:
-var room = prompt('Enter room name:');
+// var room = prompt('Enter room name:');
+//setting test room
+var room = "test";
 
 //Initializing socket.io
 var socket = io.connect();
 
 //Ask server to add in the room if room name is provided by the user
-if (room !== '') {
-    // socket.emit('create or join', room);
-    // console.log('Attempted to create or  join room', room);
+if (room !== "") {
+  // socket.emit('create or join', room);
+  // console.log('Attempted to create or  join room', room);
 }
 
 //Defining socket events
 
 //Event - Client has created the room i.e. is the first member of the room
-socket.on('created', function (room) {
-    console.log('Created room ' + room);
-    isInitiator = true;
+socket.on("created", function (room) {
+  console.log("Created room " + room);
+  isInitiator = true;
 });
 
 //Event - Room is full
-socket.on('full', function (room) {
-    console.log('Room ' + room + ' is full');
+socket.on("full", function (room) {
+  console.log("Room " + room + " is full");
 });
 
 //Event - Another client tries to join room
-socket.on('join', function (room) {
-    console.log('Another peer made a request to join room ' + room);
-    console.log('This peer is the initiator of room ' + room + '!');
-    isChannelReady = true;
+//this message is received only by the client that connected first
+//when the second peer is connected
+socket.on("join", function (room, client) {
+  console.log(
+    "Another peer made a request to join room " +
+      room +
+      " whith name :" +
+      client
+  );
+  console.log("This peer is the initiator of room " + room + "!");
+  sendmessagebutton.disabled = false;
+  isChannelReady = true;
+  remoteclient = client;
+  document.getElementById("remotename").innerHTML=client
+  socket.emit("creatorname", room, clientName);
+});
+
+socket.on("mynameis", (client) => {
+  console.log("The creator's name is " + client);
+  remoteclient = client;
+  document.getElementById("remotename").innerHTML=client
 });
 
 //Event - Client has joined the room
-socket.on('joined', function (room) {
-    console.log('joined: ' + room);
-    isChannelReady = true;
+//this message is received only by the client that connected second
+socket.on("joined", function (room) {
+  console.log("joined: " + room);
+  isChannelReady = true;
+  sendmessagebutton.disabled = false;
 });
 
 //Event - server asks to log a message
-socket.on('log', function (array) {
-    console.log.apply(console, array);
+socket.on("log", function (array) {
+  console.log.apply(console, array);
 });
-
 
 //Event - for sending meta for establishing a direct connection using WebRTC
 //The Driver code
-socket.on('message', function (message, room) {
-    console.log('Client received message:', message, room);
-    if (message === 'gotuser') {
-        maybeStart();
-    } else if (message.type === 'offer') {
-        if (!isInitiator && !isStarted) {
-            maybeStart();
-        }
-        pc.setRemoteDescription(new RTCSessionDescription(message));
-        doAnswer();
-    } else if (message.type === 'answer' && isStarted) {
-        pc.setRemoteDescription(new RTCSessionDescription(message));
-    } else if (message.type === 'candidate' && isStarted) {
-        var candidate = new RTCIceCandidate({
-            sdpMLineIndex: message.label,
-            candidate: message.candidate
-        });
-        pc.addIceCandidate(candidate);
-    } else if (message === 'bye' && isStarted) {
-        handleRemoteHangup();
+socket.on("message", function (message, room) {
+  console.log("Client received message:", message, room);
+  if (message === "gotuser") {
+    maybeStart();
+  } else if (message.type === "offer") {
+    if (!isInitiator && !isStarted) {
+      maybeStart();
     }
+    pc.setRemoteDescription(new RTCSessionDescription(message));
+    doAnswer();
+  } else if (message.type === "answer" && isStarted) {
+    pc.setRemoteDescription(new RTCSessionDescription(message));
+  } else if (message.type === "candidate" && isStarted) {
+    var candidate = new RTCIceCandidate({
+      sdpMLineIndex: message.label,
+      candidate: message.candidate,
+    });
+    pc.addIceCandidate(candidate);
+  } else if (message === "bye" && isStarted) {
+    handleRemoteHangup();
+  }
 });
-
-
 
 //Function to send message in a room
 function sendMessage(message, room) {
-    console.log('Client sending message: ', message, room);
-    socket.emit('message', message, room);
+  console.log("Client sending message: ", message, room);
+  socket.emit("message", message, room);
 }
-
-
 
 //If initiator, create the peer connection
 function maybeStart() {
-    console.log('>>>>>>> maybeStart() ', isStarted, isChannelReady);
-    if (!isStarted && isChannelReady) {
-        console.log('>>>>>> creating peer connection');
-        createPeerConnection();
-        isStarted = true;
-        console.log('isInitiator', isInitiator);
-        if (isInitiator) {
-            doCall();
-        }
+  console.log(">>>>>>> maybeStart() ", isStarted, isChannelReady);
+  if (!isStarted && isChannelReady) {
+    console.log(">>>>>> creating peer connection");
+    createPeerConnection();
+    isStarted = true;
+    console.log("isInitiator", isInitiator);
+    if (isInitiator) {
+      doCall();
     }
+  }
 }
 
 //Sending bye if user closes the window
 window.onbeforeunload = function () {
-    sendMessage('bye', room);
+  sendMessage("bye", room);
 };
-var datachannel
+var datachannel;
 //Creating peer connection
 function createPeerConnection() {
-    try {
-        pc = new RTCPeerConnection(pcConfig);
-        pc.onicecandidate = handleIceCandidate;
-        console.log('Created RTCPeerConnnection');
+  try {
+    pc = new RTCPeerConnection(pcConfig);
+    pc.onicecandidate = handleIceCandidate;
+    console.log("Created RTCPeerConnnection");
 
-        // Offerer side
-        datachannel = pc.createDataChannel("filetransfer");
-        datachannel.onopen =  (event) => {
-            datachannel.send("oferer sent:THIS")
-        };
+    // Offerer side
+    datachannel = pc.createDataChannel("filetransfer");
+    datachannel.onopen = (event) => {
+      //datachannel.send("oferer sent:THIS")
+    };
 
-        datachannel.onmessage =  (event)=> {
-            console.log("The oferrer received a message"+event.data);
+    datachannel.onmessage = (event) => {
+      console.log("The oferrer received a message" + event.data);
+    };
+    datachannel.onerror = (error) => {
+      //console.log("Data Channel Error:", error);
+    };
+
+    datachannel.onclose = () => {
+      //console.log("Data Channel closed");
+    };
+
+    // Answerer side
+    pc.ondatachannel = function (event) {
+      var channel = event.channel;
+      channel.onopen = function (event) {
+        channel.send("ANSWEREROPEN");
+      };
+      channel.onmessage = async (event) => {
+        try {
+          var themessage = event.data;
+          console.log(themessage, event);
+          viewmsgtoelement(document.getElementById("messagesent"), themessage);
+        } catch (err) {
+          console.log(err);
         }
-        datachannel.onerror = (error) => {
-            //console.log("Data Channel Error:", error);
-        };
-
-        datachannel.onclose = () => {
-            //console.log("Data Channel closed");
-        };
-
-
-        // Answerer side
-        pc.ondatachannel = function (event) {
-            var channel = event.channel;
-            channel.onopen = function (event) {
-                channel.send('ANSWEREROPEN');
-            }
-
-            const receivedBuffers = [];
-            channel.onmessage = async (event) => {
-                console.log("The Answerrer received a message"+event.data);
-                const { data } = event;
-                try {
-                    if (data !== END_OF_FILE_MESSAGE) {
-                        receivedBuffers.push(data);
-                    } else {
-                        const arrayBuffer = receivedBuffers.reduce((acc, arrayBuffer) => {
-                            const tmp = new Uint8Array(acc.byteLength + arrayBuffer.byteLength);
-                            tmp.set(new Uint8Array(acc), 0);
-                            tmp.set(new Uint8Array(arrayBuffer), acc.byteLength);
-                            return tmp;
-                        }, new Uint8Array());
-                        const blob = new Blob([arrayBuffer]);
-                        channel.send("THE FILE IS READYYY")
-                        downloadFile(blob, channel.label);
-                        channel.close();
-                    }
-                } catch (err) {
-                    console.log('File transfer failed');
-                }
-            };
-        };
-    } catch (e) {
-        console.log('Failed to create PeerConnection, exception: ' + e.message);
-        alert('Cannot create RTCPeerConnection object.');
-        return;
-    }
+      };
+    };
+  } catch (e) {
+    console.log("Failed to create PeerConnection, exception: " + e.message);
+    alert("Cannot create RTCPeerConnection object.");
+    return;
+  }
 }
 
 //Function to handle Ice candidates generated by the browser
 function handleIceCandidate(event) {
-    console.log('icecandidate event: ', event);
-    if (event.candidate) {
-        sendMessage({
-            type: 'candidate',
-            label: event.candidate.sdpMLineIndex,
-            id: event.candidate.sdpMid,
-            candidate: event.candidate.candidate
-        }, room);
-    } else {
-        console.log('End of candidates.');
-    }
+  console.log("icecandidate event: ", event);
+  if (event.candidate) {
+    sendMessage(
+      {
+        type: "candidate",
+        label: event.candidate.sdpMLineIndex,
+        id: event.candidate.sdpMid,
+        candidate: event.candidate.candidate,
+      },
+      room
+    );
+  } else {
+    console.log("End of candidates.");
+  }
 }
 
 function handleCreateOfferError(event) {
-    console.log('createOffer() error: ', event);
+  console.log("createOffer() error: ", event);
 }
 
 //Function to create offer
 function doCall() {
-    console.log('Sending offer to peer');
-    pc.createOffer(setLocalAndSendMessage, handleCreateOfferError);
+  console.log("Sending offer to peer");
+  pc.createOffer(setLocalAndSendMessage, handleCreateOfferError);
 }
 
 //Function to create answer for the received offer
 function doAnswer() {
-    console.log('Sending answer to peer.');
-    pc.createAnswer().then(
-        setLocalAndSendMessage,
-        onCreateSessionDescriptionError
-    );
+  console.log("Sending answer to peer.");
+  pc.createAnswer().then(
+    setLocalAndSendMessage,
+    onCreateSessionDescriptionError
+  );
 }
 
 //Function to set description of local media
 function setLocalAndSendMessage(sessionDescription) {
-    pc.setLocalDescription(sessionDescription);
-    console.log('setLocalAndSendMessage sending message', sessionDescription);
-    sendMessage(sessionDescription, room);
+  pc.setLocalDescription(sessionDescription);
+  console.log("setLocalAndSendMessage sending message", sessionDescription);
+  sendMessage(sessionDescription, room);
 }
 
 function onCreateSessionDescriptionError(error) {
-    trace('Failed to create session description: ' + error.toString());
+  trace("Failed to create session description: " + error.toString());
 }
 
-
 function hangup() {
-    console.log('Hanging up.');
-    stop();
-    sendMessage('bye', room);
+  console.log("Hanging up.");
+  stop();
+  sendMessage("bye", room);
 }
 
 function handleRemoteHangup() {
-    console.log('Session terminated.');
-    stop();
-    isInitiator = false;
+  console.log("Session terminated.");
+  stop();
+  isInitiator = false;
 }
 
 function stop() {
-    isStarted = false;
-    pc.close();
-    pc = null;
+  isStarted = false;
+  pc.close();
+  pc = null;
 }
 
-
-var connectbutton = document.getElementById("connectbutton")
+var connectbutton = document.getElementById("connectbutton");
 if (connectbutton) {
-    connectbutton.addEventListener("click", () => {
-        if (connectbutton.innerHTML !== "Connected") {
-            socket.emit("create or join", room);
-            sendMessage("gotuser", room);
-            if (isInitiator) {
-                maybeStart();
-            }
-        }
-        connectbutton.innerHTML = "Connected";
-        //connection logic
-    })
+  connectbutton.addEventListener("click", () => {
+    if (connectbutton.innerHTML !== "Connected") {
+      socket.emit("create or join", room, clientName);
+      sendMessage("gotuser", room);
+      if (isInitiator) {
+        maybeStart();
+      }
+    }
+    connectbutton.innerHTML = "Connected";
+    //connection logic
+  });
 }
 
-let file;
+let messagetexted = "";
 //DOM elements
-var fileInput = document.getElementById("inputfile");
-fileInput.addEventListener("change", (event) => {
-    file = event.target.files[0];
-})
-var sharefilebutton = document.getElementById("sharefile")
-sharefilebutton.addEventListener("click", () => {
-    shareFile()
-})
 
-const MAXIMUM_MESSAGE_SIZE = 65535;
-const END_OF_FILE_MESSAGE = 'EOF';
+var messageinput = document.getElementById("messagearea");
+if (messageinput) {
+  //Tip: This event is similar to the onchange event.
+  //The difference is that the oninput event occurs immediately
+  // after the value of an element has changed, while onchange occurs
+  //when the element loses focus, after the content has been changed.
+  //The other difference is that the onchange event also works on <select> elements.
+  messageinput.addEventListener("input", (event) => {
+    console.log(event.target.value);
+    messagetexted = event.target.value;
+  });
+}
 
-const shareFile = async () => {
-    console.log("Share file")
-    if (file) {
-        const arrayBuffer = await file.arrayBuffer();
-        console.log(arrayBuffer)
-        for (let i = 0; i < arrayBuffer.byteLength; i += MAXIMUM_MESSAGE_SIZE) {
-            datachannel.send(arrayBuffer.slice(i, i + MAXIMUM_MESSAGE_SIZE));
-        }
-        datachannel.send(END_OF_FILE_MESSAGE);
-    }
-};
+var sendmessagebutton = document.getElementById("sendmessage");
+if (sendmessagebutton) {
+  sendmessagebutton.disabled = true;
+  sendmessagebutton.addEventListener("click", () => {
+    var themessage = "<p>" + clientName + ":" + messagetexted + "</p>";
+    viewmsgtoelement(document.getElementById("messagesent"), themessage);
+    datachannel.send(themessage);
+    messageinput.value = "";
+    messagetexted = "";
+  });
+}
 
-const downloadFile = (blob, fileName) => {
-    const a = document.createElement('a');
-    const url = window.URL.createObjectURL(blob);
-    a.href = url;
-    a.download = fileName;
-    a.click();
-    window.URL.revokeObjectURL(url);
-    a.remove()
-};
+function viewmsgtoelement(element, message) {
+  element.innerHTML += "\n" + message;
+}
--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 //Loading dependencies & initializing express
-var os = require('os');
-var express = require('express');
+var os = require("os");
+var express = require("express");
 var app = express();
-var http = require('http');
+var http = require("http");
 //For signalling in WebRTC
-var socketIO = require('socket.io');
+var socketIO = require("socket.io");
 
+app.use(express.static("public"));
 
-app.use(express.static('public'))
-
-app.get("/", function(req, res){
-	res.render("index.ejs");
+app.get("/", function (req, res) {
+  res.render("index.ejs");
 });
 
 var server = http.createServer(app);
@@ -19,62 +18,67 @@ server.listen(process.env.PORT || 3000);
 
 var io = socketIO(server);
 
-io.sockets.on('connection', function(socket) {
+io.sockets.on("connection", function (socket) {
+  // Convenience function to log server messages on the client.
+  // Arguments is an array like object which contains all the arguments of log().
+  // To push all the arguments of log() in array, we have to use apply().
+  function log() {
+    var array = ["Message from server:"];
+    array.push.apply(array, arguments);
+    socket.emit("log", array);
+  }
 
-	// Convenience function to log server messages on the client.
-	// Arguments is an array like object which contains all the arguments of log(). 
-	// To push all the arguments of log() in array, we have to use apply().
-	function log() {
-	  var array = ['Message from server:'];
-	  array.push.apply(array, arguments);
-	  socket.emit('log', array);
-	}
-  
-    
-    //Defining Socket Connections
-    socket.on('message', function(message, room) {
-	  log('Client said: ', message);
-	  // for a real app, would be room-only (not broadcast)
-	  socket.in(room).emit('message', message, room);
-	});
-  
-	socket.on('create or join', function(room) {
-	  log('Received request to create or join room ' + room);
-  
-      var clientsInRoom = io.sockets.adapter.rooms.get(room);
-
-      var numClients = clientsInRoom ? clientsInRoom.size : 0;
-      log("Room " + room + " now has " + numClients + " client(s)")
-  
-	  if (numClients === 0) {
-		socket.join(room);
-		log('Client ID ' + socket.id + ' created room ' + room);
-		socket.emit('created', room, socket.id);
-  
-	  } else if (numClients === 1) {
-		log('Client ID ' + socket.id + ' joined room ' + room);
-		io.sockets.in(room).emit('join', room);
-		socket.join(room);
-		socket.emit('joined', room, socket.id);
-		io.sockets.in(room).emit('ready');
-	  } else { // max two clients
-		socket.emit('full', room);
-	  }
-	});
-  
-	socket.on('ipaddr', function() {
-	  var ifaces = os.networkInterfaces();
-	  for (var dev in ifaces) {
-		ifaces[dev].forEach(function(details) {
-		  if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
-			socket.emit('ipaddr', details.address);
-		  }
-		});
-	  }
-	});
-  
-	socket.on('bye', function(){
-	  console.log('received bye');
-	});
-  
+  //Defining Socket Connections
+  socket.on("message", function (message, room) {
+    log("Client said: ", message);
+    // for a real app, would be room-only (not broadcast)
+    socket.in(room).emit("message", message, room);
   });
+
+  socket.on("create or join", function (room, clientName) {
+    log("Received request to create or join room " + room);
+
+    var clientsInRoom = io.sockets.adapter.rooms.get(room);
+
+    var numClients = clientsInRoom ? clientsInRoom.size : 0;
+    log("Room " + room + " now has " + numClients + " client(s)");
+
+    if (numClients === 0) {
+      socket.join(room);
+      log("Client ID " + socket.id + " created room " + room);
+      socket.emit("created", room, socket.id);
+    } else if (numClients === 1) {
+      log("Client ID " + socket.id + " joined room " + room);
+      //this message ("join") will be received only by the first client since the client has not joined the room yet
+      io.sockets.in(room).emit("join", room, clientName); //this client name is the name of the second client who wants to join
+      socket.join(room);
+      //this mesage will be received by the second client
+      socket.emit("joined", room, socket.id);
+      //this message will be received by two cleints after the join of the second client
+      io.sockets.in(room).emit("ready");
+    } else {
+      // max two clients
+      socket.emit("full", room);
+    }
+  });
+
+  socket.on("creatorname", (room,client) => {
+    // to all clients in room1 except the sender
+    socket.to(room).emit("mynameis",client);
+  });
+
+  socket.on("ipaddr", function () {
+    var ifaces = os.networkInterfaces();
+    for (var dev in ifaces) {
+      ifaces[dev].forEach(function (details) {
+        if (details.family === "IPv4" && details.address !== "127.0.0.1") {
+          socket.emit("ipaddr", details.address);
+        }
+      });
+    }
+  });
+
+  socket.on("bye", function () {
+    console.log("received bye");
+  });
+});
